3114436af6e0225c47372266efd709b103055dc2
mithridates <84065796+mithridates1313@users.noreply.github.com>
2021-10-06 14:52:34 +0330

    Update Proxyshell.py
    fixed all issues;

--- Proxyshell.py
+++ Proxyshell.py
@@ -1,14 +1,14 @@
 import urllib3
 import requests
 import sys
+import re
 requests.packages.urllib3.disable_warnings()
 def scanner(ip):
     ip = ip.strip()
-    url = f"https://{ip}/autodiscover/autodiscover.json?@mss.com/owa/?&Email=autodiscover/autodiscover.json%3F@mss.com"
+    url = f"https://{ip}/autodiscover/autodiscover.json?@test.com/owa/?&Email=autodiscover/autodiscover.json%3F@test.com"
     try:
-        req = requests.get(url, timeout=10, verify=False, allow_redirects=False).text.split("\n")
-        print (req)
-        if (req.status_code == 302) and req.search("errorfe.aspx",req.txt):
+        req = requests.get(url, timeout=10, verify=False, allow_redirects=False)
+        if (req.status_code == 302) and (re.search("errorfe.aspx", req.text)):
             print (f"[+] {ip} Host is vulnerable ")
         else:
             print (f"[-] {ip} Host is not vulnerable")
